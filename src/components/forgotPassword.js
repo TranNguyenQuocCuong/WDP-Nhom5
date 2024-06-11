ed4d159264ec371c021da10762d3b5f6ee6a3420
phanDinhDan <phandinhdan6602@gmail.com>
2024-06-11 14:10:49 +0700

    loi frontend

--- src/components/forgotPassword.js
+++ src/components/forgotPassword.js
@@ -1,29 +1,25 @@
-import React from 'react'
-import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import axios from 'axios'
-
+import React, { useState } from 'react';
+import { useNavigate } from "react-router-dom";
+import axios from 'axios';
 
 function ForgotPassword() {
-    const [email, setEmail] = useState()
-    const navigate = useNavigate()
+  const [email, setEmail] = useState('');
+  const navigate = useNavigate();
 
-    axios.defaults.withCredentials = true;
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        axios.post('http://localhost:5000/forgotpassword', {email})
-        .then(res => {
-            if(res.data.Status === "Success") {
-                navigate('/login')
-               
-            }
-        }).catch(err => console.log(err))
-    }
+  axios.defaults.withCredentials = true;
 
-    
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post('http://localhost:5000/forgotpassword', { email })
+      .then(res => {
+        if (res.data.Status === "Success") {
+          navigate('/login');
+        }
+      }).catch(err => console.log(err));
+  };
 
-    return(
-        <div className="d-flex justify-content-center align-items-center bg-secondary vh-100">
+  return (
+    <div className="d-flex justify-content-center align-items-center bg-secondary vh-100">
       <div className="bg-white p-3 rounded w-25">
         <h4>Forgot Password</h4>
         <form onSubmit={handleSubmit}>
@@ -38,16 +34,16 @@ function ForgotPassword() {
               name="email"
               className="form-control rounded-0"
               onChange={(e) => setEmail(e.target.value)}
+              value={email}
             />
           </div>
           <button type="submit" className="btn btn-success w-100 rounded-0">
             Send
           </button>
-          </form>
-        
+        </form>
       </div>
     </div>
-    )
+  );
 }
 
 export default ForgotPassword;
